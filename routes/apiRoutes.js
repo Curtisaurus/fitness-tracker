b0d958e0926a6843ba4c6f841e16dd89552e2746
Curtisaurus <ccdrullinger@gmail.com>
2021-08-22 15:44:57 -0400

    added api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+
+// gets all workouts
+router.get("/workouts", (req, res) => {
+  Workout.find({}).sort('day')
+    .then((workoutdocs) => {
+      res.json(workoutdocs);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// creates a new workout
+router.post("/workouts", (req, res) => {
+  Workout.create(req.body)
+  .then((workoutdocs) => {
+    res.json(workoutdocs);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+});
+
+// updates a workout with an added exercise and return the new doc
+router.put("/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+  .then((workoutdocs) => {
+    res.json(workoutdocs);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+});
+
+// gets all workouts in 7-day range for api function getWorkoutsInRange
+router.get("/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration"
+        }
+      }
+    },
+    {
+      $sort: {
+        'day': -1
+      }
+    },
+    {
+      $limit: 7
+    }
+  ])
+  .then((workoutdocs) => {
+    console.log(workoutdocs)
+    res.json(workoutdocs);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+});
+
+module.exports = router;
